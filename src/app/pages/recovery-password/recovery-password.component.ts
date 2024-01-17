44cb24aa13b8af037f9d36ee8e5d1630f76f41e2
Jhon Gómez Marín <gmezjhon@gmail.com>
2024-01-16 19:38:23 -0500

    Create components login, register and recovery

---
+++ src/app/pages/recovery-password/recovery-password.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recovery-password',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recovery-password.component.html',
+  styleUrls: ['./recovery-password.component.scss']
+})
+export class RecoveryPasswordComponent {
+
+}
